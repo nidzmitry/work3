f4bba2d028d2808930bccf676138ab6e12364df9
Dzmitry <dmitry@ip-192-168-0-2.eu-central-1.compute.internal>
2023-03-01 20:43:32 +0300

    Add tests

---
+++ work3/src/test/java/GitHubPageTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubPageTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+    }
+
+    @Test
+    void TextTest() {
+        $("[placeholder='Search GitHub']").setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+
+
+        $("#wiki-pages-filter").setValue("Soft");
+        $x("//a[text()='SoftAssertions']").click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+        sleep(5000);
+
+    }
+}
+
+
